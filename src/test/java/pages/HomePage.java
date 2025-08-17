2b87a36299c5f60b863019c1eb272e7b495bad1e
nkorkin97 <korkinnikita2208@gmail.com>
2025-08-17 13:40:47 +0300

    checkbox done?

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,13 +1,10 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import config.BaseTest;
 import elements.Group;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 
--- src/test/java/pages/elementapages/CheckBoxPage.java
+++ src/test/java/pages/elementapages/CheckBoxPage.java
@@ -1,6 +1,7 @@
 package pages.elementapages;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import elements.Checked;
 import elements.State;
@@ -8,45 +9,38 @@ import org.assertj.core.api.Assertions;
 
 import static com.codeborne.selenide.Selenide.*;
 import static elements.Checked.*;
-import static elements.State.COLLAPSED;
 import static elements.State.EXPANDED;
 
 public class CheckBoxPage {
 
-    public SelenideElement findAncestorToggle(String nodeName) {
-        SelenideElement toggle = $$(".rct-node-parent")
-                .findBy(Condition.text(nodeName))
-                .$("button");
+    public SelenideElement findAncestorNode(String nodeName) {
+        SelenideElement node = $$(".rct-node-parent")
+                .findBy(Condition.text(nodeName));
+        return node;
+    }
+
+    public SelenideElement findChildNode(SelenideElement element, String childNodeName) {
+        SelenideElement node = element.$$(".rct-node-parent")
+                .findBy(Condition.text(childNodeName));
+        return node;
+    }
+    public SelenideElement findToggle(SelenideElement element) {
+        SelenideElement toggle = element.$("button");
         return toggle;
     }
 
-    public SelenideElement findChildToggle(String childNodeName) {
-        SelenideElement toggle = $$(".rct-node-parent .rct-node-parent")
-                .findBy(Condition.text(childNodeName)).$("button");
-        return toggle;
-    }
-
-    public CheckBoxPage checkToggleExpanded(SelenideElement element, State state) {
+    public CheckBoxPage checkToggleState(SelenideElement element, State state) {
         String cssClass = state.getCssClass();
         Assertions.assertThat(element.closest("li").getAttribute("class")).contains(cssClass);
         return this;
     }
 
-    public SelenideElement findAncestorCheckBox(String nodeName) {
-        SelenideElement checkbox = $$(".rct-node-parent")
-                .findBy(Condition.text(nodeName))
-                .$("#tree-node-home").ancestor("label");
+    public SelenideElement findCheckBox(SelenideElement element) {
+        SelenideElement checkbox = element.$("label");
         return checkbox;
     }
 
-    public SelenideElement findChildCheckBox(String childNodeName) {
-        SelenideElement checkbox = $$(".rct-node-parent .rct-node-parent")
-                .findBy(Condition.text(childNodeName))
-                .$("label");
-        return checkbox;
-    }
-
-    public CheckBoxPage checkBoxCheck(SelenideElement element, Checked checked) {
+    public CheckBoxPage checkBoxState(SelenideElement element, Checked checked) {
         String cssClass = checked.getCssClass();
         Assertions
                 .assertThat(element
@@ -56,14 +50,21 @@ public class CheckBoxPage {
         return this;
     }
 
+    public SelenideElement findLeaf(SelenideElement element, String leafName) {
+        SelenideElement leaf = element.$$(".rct-node-leaf").findBy(Condition.text(leafName));
+        return leaf;
+    }
+
     public void test() {
-        findAncestorToggle("Home").click();
-        findChildToggle("Downloads").click();
-        checkToggleExpanded(findAncestorToggle("Home"), EXPANDED);
-        findChildCheckBox("Desktop").click();
-        checkBoxCheck(findChildCheckBox( "Desktop"), CHECK);
-        checkBoxCheck(findAncestorCheckBox("Home"), HALFCHECK);
-        checkBoxCheck(findChildCheckBox("Downloads"), UNCHECK);
+        SelenideElement homeNode = findAncestorNode("Home");
+        SelenideElement documentsNode = findChildNode(homeNode, "Documents");
+        SelenideElement officeNode = findChildNode(documentsNode, "Office");
+        SelenideElement leaf = findLeaf(officeNode, "Public");
+
+        findToggle(homeNode).click();
+        findToggle(documentsNode).click();
+        findToggle(officeNode).click();
+        findCheckBox(leaf).click();
     }
 
 
